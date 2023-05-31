531efc86980ecbe06b15766961b64d6f1f9aa389
Jessica <jjsepulveda91@gmail.com>
2023-05-30 21:39:33 -0400

    backend and readme for reducer hook

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+  products: [
+    {
+      name: 'Rose & Sugar Body Scrub',
+      slug: 'rose-sugar-body-scrub',
+      category: 'Body Scrubs',
+      image: '/images/rs1.jpg',
+      size: '8oz',
+      price: 20,
+      rating: 5,
+      numReviews: 10,
+      description:
+        '8oz Body Scrub, Sugar Scrub, Salt Scrub, All Natural Body Scrubs, Handmade Scrubs, Pure Cane Sugar Scrub, Himalayan Salt Scrub',
+    },
+    {
+      name: 'Eucalyptus & Peppermint Body Scrub',
+      slug: 'eucalyptus-peppermint-body-scrub',
+      category: 'Body Scrubs',
+      image: '/images/ep1.jpg',
+      size: '8oz',
+      price: 20,
+      rating: 5,
+      numReviews: 10,
+      description:
+        '8oz Body Scrub, Sugar Scrub, Salt Scrub, All Natural Body Scrubs, Handmade Scrubs, Pure Cane Sugar Scrub, Himalayan Salt Scrub',
+    },
+    {
+      name: 'Lavender & Salt Body Scrub',
+      slug: 'lavender-salt-body-scrub',
+      category: 'Body Scrubs',
+      image: '/images/ls1.jpg',
+      size: '8oz',
+      price: 20,
+      rating: 5,
+      numReviews: 10,
+      description:
+        '8oz Body Scrub, Sugar Scrub, Salt Scrub, All Natural Body Scrubs, Handmade Scrubs, Pure Cane Sugar Scrub, Himalayan Salt Scrub',
+    },
+    {
+      name: 'Peppermint & Lemongrass Body Scrub',
+      slug: 'peppermint-lemongrass-body-scrub',
+      category: 'Body Scrubs',
+      image: '/images/pl1.jpg',
+      size: '8oz',
+      price: 20,
+      rating: 5,
+      numReviews: 10,
+      description:
+        '8oz Body Scrub, Sugar Scrub, Salt Scrub, All Natural Body Scrubs, Handmade Scrubs, Pure Cane Sugar Scrub, Himalayan Salt Scrub',
+    },
+  ],
+};
+
+export default data;
